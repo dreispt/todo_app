efe7536294c2b1fd417ad7bd1fb7e42e3f667e9f
Daniel Reis <dgreis@sapo.pt>
2014-09-29 22:17:07 +0100

    Create valid Odoo module

---
+++ todo_app/__openerp__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'To-Do Application',
+    'description': 'Manage your personal Tasks with this module.',
+    'author': 'Daniel Reis',
+    'depends': ['mail'],
+    'application': True,
+    'data': [
+        'todo_view.xml',
+    ]
+}
---
+++ todo_app/todo_model.py
@@ -0,0 +1,8 @@
+from openerp import models, fields
+
+
+class TodoTask(models.Model):
+    _name = 'todo.task'
+    name = fields.Char('Description', required=True)
+    is_done = fields.Boolean('Done?')
+    active = fields.Boolean('Active?', default=True)
