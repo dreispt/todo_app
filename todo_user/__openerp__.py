132679c723690e1df610b0c9a19cf9a287250e59
Daniel Reis <dgreis@sapo.pt>
2014-11-04 14:45:52 +0000

    Add XML data

--- todo_user/__openerp__.py
+++ todo_user/__openerp__.py
@@ -4,5 +4,5 @@
     'author': 'Daniel Reis',
     'depends': ['todo_app'],
     'data': ['todo_view.xml', 'security/todo_access_rules.xml'],
-    'demo': ['todo.task.csv'],
+    'demo': ['todo.task.csv', 'todo_data.xml'],
 }
