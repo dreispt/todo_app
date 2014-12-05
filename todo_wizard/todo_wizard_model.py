385773c1faeee63c2dbc1f5418d6a4480bf50dae
Daniel Reis <dgreis@sapo.pt>
2014-12-05 00:12:26 +0000

    wip

--- todo_wizard/todo_wizard_model.py
+++ todo_wizard/todo_wizard_model.py
@@ -1,4 +1,7 @@
 from openerp import models, fields, api
+from openerp import exceptions
+
+
 
 
 class TodoWizard(models.TransientModel):
@@ -11,10 +14,36 @@ class TodoWizard(models.TransientModel):
     def button_test(self):
         print "Entering interactive debugger."
         print "Press 'h' for help, 'c' to continue."
-        from pprint import pprint as pp
-        import pdb; pdb.set_trace()
+        result = True
+        from pprint import pprint as pp  # noqa
+        import pdb; pdb.set_trace()  # noqa
+        return result
 
     @api.one
-    def do_set_deadline(self):
+    def do_populate(self):
+        Todo = self.env['todo.task']
+        all_todos = Todo.search([])
+        #self.tasks = all_todos
+        # Reopen wizard
+        action = {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            #'res_id': self.id,
+            'res_model': 'todo.wizard', #self._name,
+            'target': 'new',
+        }
+        print action
+        return self.env.ref('todo_app.action_todo_wizard')
+        return action
+
+
+    @api.one
+    def do_mass_update(self):
+        if not self.new_deadline and not self.new_user:
+            raise exceptions.ValidationError('No data to update!')
         for task in self.tasks:
-            task.date_deadline = self.new_deadline
+            if self.new_deadline:
+                task.date_deadline = self.new_deadline
+            if self.new_user:
+                task.user_id = self.new_user
