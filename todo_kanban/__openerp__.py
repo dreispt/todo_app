abcb8d3a32fa206fbfdcd631e1e8d193c793fc43
Daniel Reis <dgreis@sapo.pt>
2014-12-17 00:44:25 +0000

    small change

--- todo_kanban/__openerp__.py
+++ todo_kanban/__openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': 'To-Do Kanban board',
-    'description': 'Kanban board to manage todo tasks.',
+    'description': 'Kanban board to manage to-do tasks.',
     'author': 'Daniel Reis',
     'depends': ['todo_ui'],
     'data': ['todo_view.xml'],
