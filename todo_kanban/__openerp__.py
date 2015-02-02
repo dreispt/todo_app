cc24ef69942187ee11ad8102064cd4071a93910d
Daniel Reis <dgreis@sapo.pt>
2015-02-02 09:32:31 +0000

    Ch08 QWeb, Kanban views and Reports

---
+++ todo_kanban/__openerp__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'To-Do Kanban board',
+    'description': 'Kanban board to manage to-do tasks.',
+    'author': 'Daniel Reis',
+    'depends': ['todo_ui', 'report'],
+    'data': [
+        # 'todo_view_vignette.xml',
+        'todo_view.xml',
+        'todo_report.xml',
+    ],
+}
