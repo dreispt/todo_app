b20cfb10c66ad1d4c4ac3ef51e634febd5c71f62
Daniel Reis <dreis.pt@hotmail.com>
2015-11-17 23:36:26 +0000

    Add tests

--- todo_app/__openerp__.py
+++ todo_app/__openerp__.py
@@ -2,7 +2,7 @@
     'name': 'To-Do Application',
     'description': 'Manage your personal Tasks with this module.',
     'author': 'Daniel Reis',
-    'depends': ['mail'],
+    'depends': ['base'],
     'application': True,
     'data': [
         'todo_view.xml',
---
+++ todo_app/tests/test_todo.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from openerp.tests.common import TransactionCase
+
+
+class TestTodo(TransactionCase):
+
+    def setUp(self, *args, **kwargs):
+        result = super(TestTodo, self).setUp(*args, **kwargs)
+        user_demo = self.browse_ref('base.user_demo')
+        self.env= self.env(user=user_demo)
+        return result
+
+    def test_create(self):
+        "Create a simple Todo."
+        Todo = self.env['todo.task']
+        # Create Test Task
+        task = Todo.create({'name': 'Test Task'})
+        self.assertEqual(task.is_done, False)
+        # Test Toggle Done
+        task.do_toggle_done()
+        self.assertEqual(task.is_done, True)
+        # Test Clear Done
+        task.do_clear_done()
+        self.assertEqual(task.active, False)
