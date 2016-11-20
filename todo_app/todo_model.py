9bf67b279abae2dc75e169a53be5d6d03f38782c
Daniel Reis <dreis.pt@hotmail.com>
2016-11-20 16:01:34 +0000

    Port Ch02 to 10.0

--- todo_app/todo_model.py
+++ todo_app/todo_model.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields, api
+from odoo import models, fields, api
 
 
 class TodoTask(models.Model):
     _name = 'todo.task'
-    _description = 'To-do task'
+    _description = 'To-do Task'
+
     name = fields.Char('Description', required=True)
     is_done = fields.Boolean('Done?')
     active = fields.Boolean('Active?', default=True)
 
-    @api.one
+    @api.multi
     def do_toggle_done(self):
-        # print self.env.context
-        self.is_done = not self.is_done
+        for task in self:
+            task.is_done = not task.is_done
         return True
 
-    @api.multi
+    @api.model
     def do_clear_done(self):
-        done_recs = self.search([('is_done', '=', True)])
-        done_recs.write({'active': False})
+        dones = self.search([('is_done', '=', True)])
+        dones.write({'active': False})
         return True
