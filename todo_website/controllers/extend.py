89b457964e6cac81a79a2dfcf60c89ebf0be7d0e
Daniel Reis <dreis.pt@hotmail.com>
2018-03-11 23:28:50 +0000

    Ch12 Website

---
+++ todo_website/controllers/extend.py
@@ -0,0 +1,11 @@
+from odoo import http
+from odoo.addons.todo_website.controllers.main import Todo
+
+
+class TodoExtended(Todo):
+
+    @http.route(['/hello', '/hello/<name>'])
+    def hello(self, name=None, **kwargs):
+        response = super(TodoExtended, self).hello()
+        response.qcontext['name'] = name
+        return response
